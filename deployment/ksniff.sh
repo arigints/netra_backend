83e0f587308ceeafac957ae6f98132e845dcb0b3
arigints <gintingari73@gmail.com>
2024-05-14 12:49:20 +0700

    edit Dockerfile and ksniff installer

--- deployment/ksniff.sh
+++ deployment/ksniff.sh
@@ -2,7 +2,5 @@
 wget -P /install https://github.com/eldadru/ksniff/releases/download/v1.6.2/ksniff.zip &&
 unzip /install/ksniff.zip -d /install &&
 sed -i 's/--short=true //' /install/Makefile &&
-pwd &&
-ls &&
 make -C /install install
 )
