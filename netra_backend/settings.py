92f8e7692e21cfce1855c08200fb04867198c6c6
whiwk <bpras747@gmail.com>
2024-06-13 16:17:36 +0700

    update on token lifetime from 5min to 120min

--- netra_backend/settings.py
+++ netra_backend/settings.py
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 ##################################################################################
