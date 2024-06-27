da6a711e2fd633dbab7f80605e06f13b3c4e46a7
arigints <gintingari73@gmail.com>
2024-06-27 22:34:05 +0700

    update all completion related

--- apps/management/commands/import_all_data.sh
+++ apps/management/commands/import_all_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 # Run the Django management command to import CU data
-python ../../../manage.py import_data_cu ../data/cuconfig.xlsx
-python ../../../manage.py import_data_du ../data/duconfig.xlsx
-python ../../../manage.py import_data_ue ../data/ueconfig.xlsx
-
+python3 manage.py import_data_cu apps/management/data/cuconfig.xlsx
+python3 manage.py import_data_du apps/management/data/duconfig.xlsx
+python3 manage.py import_data_ue apps/management/data/ueconfig.xlsx
