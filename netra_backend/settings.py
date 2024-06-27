8eb8f39aa1bb5d525c49765e2c6a1d940a761311
whiwk <bpras747@gmail.com>
2024-06-27 14:24:54 +0700

    removing some django cors headers unrelevant setting

--- netra_backend/settings.py
+++ netra_backend/settings.py
@@ -205,13 +205,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "https://orca.tiplab.id/",
-]
 
 CORS_ALLOW_METHODS = [
     "GET",
